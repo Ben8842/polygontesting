cf4953e22206310c68849f383a491d38371050ce
Ben8842 <z87e99b44r55a@gmail.com>
2021-07-11 21:27:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { Component } from "react";
 
-class Apitesting extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       flag: false,
+      polygonData: [],
+      isLoaded: false,
     }
   }
+
+  componentDidMount() {
+    /*fetch(
+      "https://api.polygon.io/v2/reference/news?limit=3&order=descending&sort=published_utc&ticker=AAPL&published_utc.gte=2021-04-26&apiKey=JAVPppRs8ukmgLkgh6XmxUXKH5orOZqD"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+         
+          polygonData: json,
+          
+        });
+        console.log("complete");
+      });*/
+  }
+
+  apifetcher() {
+  
+      fetch(
+      "https://api.polygon.io/v2/reference/news?limit=3&order=descending&sort=published_utc&ticker=AMZN&published_utc.gte=2021-04-26&apiKey=JAVPppRs8ukmgLkgh6XmxUXKH5orOZqD"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+         
+          polygonData: json,
+          isLoaded: true,
+          
+        });
+      });
+    console.log("fetch complete")
+
+
+  }
+
+  stateprinter() {
+    console.log(this.state);
+    
+  }
+
+  displaynews() {
+    console.log("hello news")
+  }
+
   render() {
+    var { polygonData, isLoaded } = this.state;
+   
+   
+      const newscontainer = (
+        <div>
+          <div>TITLE: {isLoaded ? polygonData.results[0].title : "loading"}</div>
+          <div>ARTICLE URL <a href={isLoaded ? polygonData.results[2].article_url : "loading"}>{isLoaded ? polygonData.results[0].article_url : "loading"}</a></div>
+          <div>ARTICLE DESCRIPTION {isLoaded ? polygonData.results[0].description : "loading"}</div>
+          <div>BREAK BREAK BREAK</div>
+          <div>TITLE: {isLoaded ? polygonData.results[1].title : "loading"}</div>
+          <div>ARTICLE URL <a href={isLoaded ? polygonData.results[2].article_url : "loading"}>{isLoaded ? polygonData.results[1].article_url : "loading"}</a></div>
+          <div>ARTICLE DESCRIPTION {isLoaded ? polygonData.results[1].description : "loading"}</div>
+          <div>BREAK BREAK BREAK</div>
+          <div>TITLE: {isLoaded ? polygonData.results[2].title : "loading"}</div>
+          <div>ARTICLE URL <a href={isLoaded ? polygonData.results[2].article_url : "loading"}>{isLoaded ? polygonData.results[2].article_url : "loading"}</a></div>
+          <div>ARTICLE DESCRIPTION {isLoaded ? polygonData.results[2].description : "loading"}</div>
+          <div>BREAK BREAK BREAK</div>
+        </div>
+      )
+    
+
     return (
-      <div>Hello World more<div>more
-      </div></div>
+      <div>
+        <div>
+          <button onClick={() => this.apifetcher()}>TEST THE POLYGON API </button>
+        </div>
+        <div>
+          <button onClick={() => this.stateprinter()}>PRINT THE STATE</button>
+        </div>
+      
+        <div>{isLoaded ? newscontainer : null}</div>
+      </div>
     )
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-     HELLO WORLD
-    </div>
-  );
-}
+
 
 export default App;
